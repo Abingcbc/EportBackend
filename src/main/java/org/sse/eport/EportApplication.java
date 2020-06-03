88f89467fadca8f5d5f944d9fa40c41e54a1ef89
Abingcbc <19990626.love@163.com>
2020-06-03 15:48:50 +0800

    init

---
+++ src/main/java/org/sse/eport/EportApplication.java
@@ -0,0 +1,13 @@
+package org.sse.eport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sse/eport/EportApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sse.eport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
